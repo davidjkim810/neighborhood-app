4bbc9e9e86a5311d4ecbe37c2b2760c27b714c5a
David Kim <davidjkim810@gmail.com>
2018-10-11 12:17:30 -0400

    created user model, ran migration

---
+++ neighborhood-api/db/migrate/20181011161342_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :first_name
+      t.string :last_name
+      t.string :nickname
+      t.string :phone_number
+      t.string :street
+      t.string :city
+      t.string :state
+      t.integer :zip_code
+
+      t.timestamps
+    end
+  end
+end
