b6aa603646a9357b2dcd2ab39a1ad1a1bf7d8a51
David Kim <davidjkim810@gmail.com>
2018-10-12 16:48:47 -0400

    created api, v1 folder, created communities back-end api CRUD

---
+++ neighborhood-api/app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+      class ItemsController < ApplicationController
+
+      def index
+        @items = Item.all
+        render :json => @items
+      end
+
+    end
+  end
+end
--- neighborhood-api/config/routes.rb
+++ neighborhood-api/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :communities
+  namespace 'api' do
+    namespace 'v1' do
+      resources :communities
+    end
+  end
   resources :items, only: [:index]
   resources :users, only: [:show, :edit, :update, :destroy, :new, :create] do
     resources :items, only: [:show, :edit, :update, :new, :create, :destroy]
