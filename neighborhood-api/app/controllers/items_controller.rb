9fb89e956aa74298dbb892e3be824fe5cef82c26
David Kim <davidjkim810@gmail.com>
2018-10-12 11:32:52 -0400

    set up index route for items controller

--- neighborhood-api/app/controllers/items_controller.rb
+++ neighborhood-api/app/controllers/items_controller.rb
@@ -5,22 +5,4 @@ def index
   render :json => @items
 end
 
-def show
-end
-
-def new
-end
-
-def create
-end
-
-def edit
-end
-
-def update
-end
-
-def destroy
-end
-
 end
--- neighborhood-api/config/routes.rb
+++ neighborhood-api/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :communities
+  resources :items, only: [:index]
 end
