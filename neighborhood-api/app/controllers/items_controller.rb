92a8a54eeb452229d108606c1ea16914220be46f
David Kim <davidjkim810@gmail.com>
2018-10-12 11:30:49 -0400

    created communities_controller, back end api index for communities, created items_controller

---
+++ neighborhood-api/app/controllers/items_controller.rb
@@ -0,0 +1,26 @@
+class ItemsController < ApplicationController
+
+def index
+  @items = Item.all
+  render :json => @items
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+end
