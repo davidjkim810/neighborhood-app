1747d75c69c0ac00081a5aceada193bf7d4d5d72
David Kim <davidjkim810@gmail.com>
2018-10-12 12:02:46 -0400

    created user routes, created nested items route

--- neighborhood-api/config/routes.rb
+++ neighborhood-api/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :communities
   resources :items, only: [:index]
+  resources :users, only: [:show, :edit, :update, :destroy, :new, :create] do
+    resources :items, only: [:index, :edit, :update, :new, :create, :destroy]
+  end
 end
